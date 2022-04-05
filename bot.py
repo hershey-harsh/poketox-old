0d89917d55b4d7d78e61d76cb27bce0252efc6d0
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-04-04 20:36:36 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,68 +1,50 @@
-import datetime
-import asyncio
+import subprocess
+import os
+
+#Installs all the dependencies automatically.
+#The only problem is people usually have multiple virtual environments and their pip version might be different.
+
+def install(name):
+  subprocess.call(['pip', 'install', name])
+install("git+https://github.com/iDevision/enhanced-discord.py@edpy-legacy")
+
+os.system("clear")
 
 import discord
 from discord.ext import commands, events
 from discord.ext.events import member_kick
-
+import datetime
 import config
+import keep_alive
 
 COGS = [
-    "admin",
-    "bot",
-    "collectors",
-    "configuration",
-    "casino",
     "data",
-    "duel",
-    "event",
-    "fun",
-    "info",
-    "minigame",
     "mongo",
     "pokedex",
-    "reputation",
+    "collectors",
     "shinyhunt",
-    "tags",
-    "logging",
-    "utility", 
+    "identify",
+    "info",
+    "weakness",
+    "dex"
 ]
 
-async def _prefix_callable(bot, msg):
-    user_id = bot.user.id
-    base = [f'<@!{user_id}> ', f'<@{user_id}> ']
-    if msg.guild is None:
-        base.append(config.DEFAULT_PREFIX)
-    else:
-        prefix = bot.prefixes.get(msg.guild.id)
-        if prefix is None:
-            guild = await bot.mongo.fetch_guild(msg.guild)
-            bot.prefixes[msg.guild.id] = guild.prefix
-            prefix = bot.prefixes.get(msg.guild.id)
 
-        base.append(prefix)
-
-    return base
-
-class Bot(commands.AutoShardedBot, events.EventsMixin):
+class Bot(commands.Bot, events.EventsMixin):
     def __init__(self, **kwargs):
         super().__init__(
             **kwargs,
-            command_prefix= _prefix_callable,
-            allowed_mentions=discord.AllowedMentions(everyone=False, roles=False),
+            command_prefix=[config.PREFIX, "A!"], self_bot=False, slash_commands=True, intents=discord.Intents.all(),
+            allowed_mentions=discord.AllowedMentions(everyone=False, roles=False, replied_user=False),
             case_insensitive=True,
         )
-
+      
         self.config = config
-        self.prefixes = {}
 
-        self.load_extension("jishaku")
+
+
         for i in COGS:
             self.load_extension(f"cogs.{i}")
-
-    @property
-    def log(self):
-        return self.get_cog("Logging").log
         
     @property
     def mongo(self):
@@ -72,18 +54,11 @@ class Bot(commands.AutoShardedBot, events.EventsMixin):
     def data(self):
         return self.get_cog("Data").instance
 
-    # @property
-    # def cache(self):
-    #     return self.get_cog("Cache")
-
     async def on_ready(self):
-        self.log.info(f"Ready called.")
-
-    async def close(self):
-        self.log.info("Shutting down")
-        await super().close()
-
+        self.log.info(f"Logged in")
 
 if __name__ == "__main__":
     bot = Bot()
+    bot.remove_command('help')
+    keep_alive.keep_alive()
     bot.run(config.BOT_TOKEN)
