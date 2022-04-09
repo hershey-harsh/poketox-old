fc71d2379c2b5098a2d4664424d7062e4241c4fd
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-04-09 17:32:18 -0400

    Create solve.py

---
+++ solve.py
@@ -0,0 +1,16 @@
+from transformers import ViTForImageClassification, ViTFeatureExtractor
+from PIL import Image
+import torch
+
+# Loading in Model
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = ViTForImageClassification.from_pretrained( "pokemon_classifier").to(device)
+feature_extractor = ViTFeatureExtractor.from_pretrained('pokemon_classifier')
+
+# Caling the model on a test image
+def solve():
+  img = Image.open('pokemon.png')
+  extracted = feature_extractor(images=img, return_tensors='pt').to(device)
+  predicted_id = model(**extracted).logits.argmax(-1).item()
+  predicted_pokemon = model.config.id2label[predicted_id]
+  return predicted_pokemon
