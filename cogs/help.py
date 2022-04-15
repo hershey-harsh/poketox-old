43fc65cd555af8a80aade5d0c890fd8620958629
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-04-14 20:00:53 -0400

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -8,14 +8,14 @@ class Dropdown(discord.ui.Select):
         # Set the options that will be presented inside the dropdown
         options = [
             discord.SelectOption(label='Miscellaneous', description='View Pokétwo Miscellaneous commands', emoji='<:poketwo:964311966384554025>'),
-            discord.SelectOption(label='Collectors', description='View collectors commands', emoji='<:pokeball:936773252913700894>'),
+            discord.SelectOption(label='Collectors', description='View Collectors commands', emoji='<:pokeball:936773252913700894>'),
             discord.SelectOption(label='Shiny Hunt', description='View Shiny Hunt commands', emoji='✨'),
         ]
 
         # The placeholder is what will be shown when no option is chosen
         # The min and max values indicate we can only pick one of the three options
         # The options parameter defines the dropdown options. We defined this above
-        super().__init__(placeholder='Choose your favourite colour...', min_values=1, max_values=1, options=options)
+        super().__init__(placeholder='Choose the category', min_values=1, max_values=1, options=options)
 
     async def callback(self, interaction: discord.Interaction):
         # Use the interaction object to send a response message containing
