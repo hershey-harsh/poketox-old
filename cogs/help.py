edbd7eebd4b624454cddefc367e39434f65a9175
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-04-14 21:53:57 -0400

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -64,11 +64,11 @@ class Help(commands.Cog):
 
   @commands.group(invoke_without_command=True, ignore_extra=False)
   async def help(self, ctx):
-    embed=discord.Embed(title="Pokétox", description="Use the menu below to see how to use commands! Checkout Pokétox [Terms of Service](http://poketox.me/tos)")
+    embed=discord.Embed(title="Pokétox", description="Use the menu below to see how to use commands! Checkout Pokétox [Terms of Service](http://poketox.me/tos)", color=0x2F3136)
     embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/875526899386953779/d46976087eef1662db19c8272ebb57e4.png")
     embed.add_field(name="About Me", value="Poketwo reimagined — Assists you with catching, price checks pokémons, automatically pings Shiny Hunters, and much more.", inline=False)
-    embed.add_field(name="<:developer:964340773367726101>Developer", value="[Future#0811](https://discord.com/users/790788488983085056)", inline=True)
-    embed.add_field(name="🔗Links", value="[Pokétox Website](http://poketox.me/)\n[Support Server](https://discord.gg/mhcjdJkxn6)\n[Bot Invite](https://discord.com/oauth2/authorize?client_id=875526899386953779&scope=bot%20applications.commands&permissions=388168)", inline=True)
+    embed.add_field(name="<:developer:964340773367726101> Developer", value="[Future#0811](https://discord.com/users/790788488983085056)", inline=True)
+    embed.add_field(name="🔗 Links", value="[Pokétox Website](http://poketox.me/)\n[Support Server](https://discord.gg/mhcjdJkxn6)\n[Bot Invite](https://discord.com/oauth2/authorize?client_id=875526899386953779&scope=bot%20applications.commands&permissions=388168)", inline=True)
     await ctx.send(embed=embed, view=DropdownView(ctx))
     
 def setup(bot):
