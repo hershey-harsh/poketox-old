9be4e666628b850e92a7f179069b7b12f15ae1fb
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-04-15 22:38:57 -0400

    Update pokedex.py

--- cogs/pokedex.py
+++ cogs/pokedex.py
@@ -324,11 +324,6 @@ class Pokedex(commands.Cog):
                 except:
                     pass
             
-                if pokemon in pokes:
-                        await aaa.edit(embed=embed1, view=Confirm(img_url, pokemon, pokemon, self.bot))
-                else:
-                        await aaa.edit(embed=embed1, view=Confirmm(img_url, pokemon, pokemon, self.bot))
-            
                 try:
                         await collectors.collectping(self, ctx, species)
                         await collectors.shinyping(self, ctx, species)
@@ -342,11 +337,6 @@ class Pokedex(commands.Cog):
                     await message.channel.send(f'<@&{roleid}>')
                 except:
                     pass
-            
-                if pokemon in pokes:
-                        await aaa.edit(embed=embed1, view=Confirm(img_url, pokemon, pokemon, self.bot))
-                else:
-                        await aaa.edit(embed=embed1, view=Confirmm(img_url, pokemon, pokemon, self.bot))
             
                 try:
                         await collectors.collectping(self, ctx, species)
