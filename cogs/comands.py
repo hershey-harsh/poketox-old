b6aedd51454066fea42989b018bbcf39aaadadb6
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-04-18 10:57:10 -0400

    Create comands.py

---
+++ cogs/comands.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+import random
+import time
+import config
+
+import asyncio
+import requests
+import json
+import asyncio
+import datetime
+from name import solve
+import discord
+from discord.ext import commands, menus
+from helpers.converters import FetchUserConverter, SpeciesConverter
+from helpers.pagination import AsyncListPageSource
+from helpers import checks
+import asyncio
+from replit import db
+import discord,random,os
+from discord.ext import commands
+
+class comands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+   
+    @commands.command(brief="Suggest new features")
+    async def suggest(self, ctx, *, args):
+      chan = self.bot.get_channel(939162941536735292)
+      
+      embed=discord.Embed(title="💡 Suggestion", color=0x5865F2)
+      embed.add_field(name="Server", value=f"{ctx.guild} (*{ctx.guild.id}*)", inline=True)
+      embed.add_field(name="User", value=f"{ctx.author.mention} (*{ctx.author.id}*)", inline=True)
+      embed.add_field(name="Suggestion", value=f'> "{args}"', inline=False)
+      
+      await chan.send(embed=embed) 
+      
+def setup(bot):
+    print("Loaded Commands")
+    bot.add_cog(comands(bot))
