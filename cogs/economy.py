6a544fc9984e6e54d13c266823d36c4bd4efb4c6
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-04-20 10:45:08 -0400

    Update economy.py

--- cogs/economy.py
+++ cogs/economy.py
@@ -7,6 +7,17 @@ import discord
 from discord.ext import commands
 from helpers import checks, helper
 import math
+import asyncio
+import math
+import random
+from datetime import datetime, timedelta
+
+import config
+import discord
+from discord.ext import commands
+from helpers import checks, helper
+
+from data import models
 
 # from helpers.puzzle import PuzzleType, Puzzle
 
@@ -43,7 +54,169 @@ class Minigame(commands.Cog):
         embed.title = f"Balance"
         embed.add_field(name="Tokens", value=f"{amount:,}")
 
-        return await ctx.send(embed=embed)        
+        return await ctx.send(embed=embed)     
+    
+    @checks.has_started()
+    @commands.max_concurrency(1, commands.BucketType.user)
+    @commands.command(aliases=["rf", "romo"])
+    async def romoflip(self, ctx, start="about"):
+        if start == "start":
+            member = await self.bot.mongo.fetch_member_info(ctx.author)
+            amount = member.balance
+
+            if amount < 10:
+                return await ctx.send("You don't have enough tokens to play (min: 10)")
+
+            flip_dialogue = ""
+            health = 100
+            for i in range(10):
+                damage = random.randint(5, 14)
+                health -= damage
+                if health < 0:
+                    health = 0
+                flip_dialogue += f"Flip {i+1}: {damage} damage — health: {health}\n"
+                if health <= 0:
+                    break
+
+            embed = discord.Embed(
+                title=f"Flipping Romo — 10 tokens",
+                description=flip_dialogue,
+                color=0xEB4634,
+            )
+
+            if health <= 0:
+                await self.bot.mongo.update_member(
+                    ctx.author, {"$inc": {"balance": 10}}
+                )
+                embed.add_field(name="Winnings", value="You won 20 tokens!")
+            else:
+                await self.bot.mongo.update_member(
+                    ctx.author, {"$inc": {"balance": -10}}
+                )
+                embed.add_field(name="Winnings", value="You won 0 tokens.")
+            await ctx.send(f"> <@!{ctx.author.id}>", embed=embed)
+            return
+        else:
+            embed = discord.Embed(
+                title=f"Romo Flip",
+                description=f"You pay 10 tokens to flip Romo 10 times. He starts with 100 health and each time you flip him, he loses 5-14 health. If he dies, you will receive 20 tokens. Do `{ctx.prefix}romoflip start` to play!",
+                color=0xEB4634,
+            )
+            return await ctx.send(embed=embed)
+
+    @checks.has_started()
+    @commands.max_concurrency(1, commands.BucketType.user)
+    @commands.command(aliases=["dr", "dice"])
+    async def diceroll(self, ctx, start="about", amount=10):
+        if amount <= 0:
+            return await ctx.send("Nice Try")
+
+        if start == "start":
+            member = await self.bot.mongo.fetch_member_info(ctx.author)
+            bal = member.balance
+
+            if bal < amount:
+                return await ctx.send(
+                    f"You don't have enough tokens to play (min: {amount})"
+                )
+
+            outcome = config.RATES(amount)
+            if outcome:
+                dice_roll = random.randint(4, 6)
+            else:
+                dice_roll = random.randint(1, 3)
+
+            embed = discord.Embed(color=0xEB4634)
+            embed.title = f"Dice Roll — {amount}"
+            embed.add_field(name="Roll", value=f"You rolled a **{dice_roll}**.")
+            if dice_roll >= 4:
+                await self.bot.mongo.update_member(
+                    ctx.author, {"$inc": {"balance": amount}}
+                )
+                embed.add_field(
+                    name="Winnings",
+                    value=f"You won **{amount*2} tokens**!",
+                    inline=False,
+                )
+            else:
+                await self.bot.mongo.update_member(
+                    ctx.author, {"$inc": {"balance": -1 * amount}}
+                )
+                embed.add_field(
+                    name="Winnings", value=f"You won **0 tokens.**", inline=False
+                )
+            return await ctx.send(f"> <@!{ctx.author.id}>", embed=embed)
+        else:
+            embed = discord.Embed(
+                title=f"Dice Roll",
+                description=f"You roll a 6 sided dice. If the dice is at least 4, you win 2x the amount you entered with. Do `{ctx.prefix}diceroll start <amount>` to play!",
+                color=0xEB4634,
+            )
+            return await ctx.send(embed=embed)
+
+    @checks.has_started()
+    @commands.max_concurrency(1, commands.BucketType.user)
+    @commands.command(aliases=["cf", "coin"])
+    async def coinflip(self, ctx, start="about", amount=10, choice="heads"):
+        choice = choice.lower()
+        if amount <= 0:
+            return await ctx.send("Nice Try")
+
+        if choice not in ["heads", "h", "tails", "t"]:
+            return await ctx.send("Please choose a valid choice — (h)eads, (t)ails)")
+
+        if choice == "h":
+            choice = "heads"
+        if choice == "t":
+            choice = "tails"
+
+        if start == "start":
+            member = await self.bot.mongo.fetch_member_info(ctx.author)
+            bal = member.balance
+
+            if bal < amount:
+                return await ctx.send(
+                    f"You don't have enough tokens to play (min: {amount})"
+                )
+
+            outcome = config.RATES(amount)
+            if outcome:
+                flip = choice
+            else:
+                flip = "tails" if choice == "heads" else "heads"
+
+            embed = discord.Embed(color=0xEB4634)
+            embed.title = f"Coinflip — {amount}"
+            embed.add_field(
+                name="Win Condition", value=f"{choice.capitalize()}", inline=False
+            )
+            embed.add_field(
+                name="Flip", value=f"You flipped a **{flip}**.", inline=False
+            )
+            if flip == choice:
+                await self.bot.mongo.update_member(
+                    ctx.author, {"$inc": {"balance": amount}}
+                )
+                embed.add_field(
+                    name="Winnings",
+                    value=f"You won **{amount*2} tokens**!",
+                    inline=False,
+                )
+            else:
+                await self.bot.mongo.update_member(
+                    ctx.author, {"$inc": {"balance": -1 * amount}}
+                )
+                embed.add_field(
+                    name="Winnings", value=f"You won **0 tokens.**", inline=False
+                )
+            return await ctx.send(f"> <@!{ctx.author.id}>", embed=embed)
+        else:
+            embed = discord.Embed(
+                title=f"Coin Flip",
+                description=f"You flip a coin, if the coin lands on heads, you win 2x the amount you entered with. Do `{ctx.prefix}coinflip start <amount>` to play!",
+                color=0xEB4634,
+            )
+            return await ctx.send(embed=embed)
     
     @commands.group(invoke_without_command=True)
     async def spawn(self, ctx):
