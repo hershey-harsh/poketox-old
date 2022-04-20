0dd71dccff3020208db6500d0fba01fac3114c4f
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-04-20 08:59:10 -0400

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -13,7 +13,7 @@ class Admin(commands.Cog):
         self.bot = bot
     
     async def is_banker(self, ctx):
-        return ctx.guild.get_role(819688054276751381) in ctx.author.roles
+        return ctx.guild.get_role(929227238166114306) in ctx.author.roles
 
     @checks.is_banker()
     @commands.command(aliases=("susp",))
@@ -47,33 +47,7 @@ class Admin(commands.Cog):
         await self.bot.mongo.update_member(user, {"$inc": {"balance": amount}})
         await self.bot.mongo.update_member(ctx.author, {"$inc": {"banker_balance": -1*amount}})
         await ctx.send(f"Gave **{user}** {amount} tokens.")
-    
-    @commands.is_owner()
-    @commands.command(aliases = ["ap"])
-    async def addpoints(self, ctx, user: FetchUserConverter, amount=0):
-        u = await self.bot.mongo.fetch_member_info(user)
-        if u is None:
-            return await ctx.send(f"**{user.name}** needs to run `{ctx.prefix}start`")
-        elif u.suspended:
-            return await ctx.send(f"**{user.name}** is suspended")
-        elif not u.event_activated:
-            return await ctx.send(f"**{user.name}** is not in the event.")
-
-        await self.bot.mongo.update_member(user, {"$inc": {"points": amount}})
-        await ctx.send(f"Gave **{user}** {amount} points.")
-    
-    @checks.is_banker()
-    @commands.command(aliases = ["aub"])
-    async def add_untrack_bal(self, ctx, user: FetchUserConverter, amount=0):
-        u = await self.bot.mongo.fetch_member_info(user)
-        if u is None:
-            return await ctx.send(f"**{user.name}** needs to run `{ctx.prefix}start`")
-        elif u.suspended:
-            return await ctx.send(f"**{user.name}** is suspended")
-
-        await self.bot.mongo.update_member(user, {"$inc": {"balance": amount}})
-        await ctx.send(f"Gave **{user}** {amount} tokens.")
-    
+        
     @checks.is_banker()
     @commands.command(aliases = ["sb"])
     async def showbal(self, ctx, user: FetchUserConverter):
@@ -105,20 +79,6 @@ class Admin(commands.Cog):
             {"$set": {"has_collected": False}},
         )
         return await ctx.send(f"Reset eveyone's event collect!")
-    
-    @commands.is_owner()
-    @commands.command(aliases = ["abb"])
-    async def addbankerbal(self, ctx, user: FetchUserConverter, amount = 0):
-        await self.bot.mongo.update_member(user, {"$inc": {"banker_balance": amount}})
-        return await ctx.send(f"Gave **{user}** {amount} banker tokens.")
-    
-    @commands.is_owner()
-    @commands.command(aliases = ["rbb"])
-    async def resetbankerbalance(self, ctx, user: FetchUserConverter):
-        member = await self.bot.mongo.fetch_member_info(ctx.author)
-        amount = member.banker_balance*-1
-        await self.bot.mongo.update_member(user, {"$inc": {"banker_balance": amount}})
-        return await ctx.send(f"Gave **{user}** {amount} banker tokens.")
     
 def setup(bot):
     bot.add_cog(Admin(bot))
