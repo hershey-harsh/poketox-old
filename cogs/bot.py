c06ea24a38b32b03e5bfa56bd5c107186e3fe47c
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-04-20 23:01:42 -0400

    Update bot.py

--- cogs/bot.py
+++ cogs/bot.py
@@ -10,7 +10,51 @@ class Error_Hand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
    
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.send("This command cannot be used in private messages.")
+        elif isinstance(error, commands.CommandOnCooldown):
+            embed = discord.Embed(
+                title=f"Slow it down!",
+                description=f"Try again in {error.retry_after:.2f}s.",
+                color=0xEB4634
+            )
+            await ctx.send(embed=embed)
+        elif isinstance(error, commands.DisabledCommand):
+            await ctx.send("Sorry. This command is disabled and cannot be used.")
+        elif isinstance(error, commands.BotMissingPermissions):
+            missing = [
+                "`" + perm.replace("_", " ").replace("guild", "server").title() + "`"
+                for perm in error.missing_perms
+            ]
+            fmt = "\n".join(missing)
+            message = f"💥 Err, I need the following permissions to run this command:\n{fmt}\nPlease fix this and try again."
+            if ctx.me.permissions_in(ctx.channel).send_messages:
+                await ctx.send(message)
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send_help(ctx.command)
+        elif isinstance(error, commands.CheckFailure):
+            await ctx.send(error)
+        elif isinstance(error, commands.UserInputError):
+            await ctx.send(error)
+        elif isinstance(error, commands.CommandNotFound):
+            return
+        else:
+            print(f"Ignoring exception in command {ctx.command}")
+            traceback.print_exception(
+                type(error), error, error.__traceback__, file=sys.stderr
+            )
 
+    @commands.Cog.listener()
+    async def on_error(self, event, error):
+        if isinstance(error, discord.NotFound):
+            return
+        else:
+            print(f"Ignoring exception in event {event}:")
+            traceback.print_exception(
+                type(error), error, error.__traceback__, file=sys.stderr
+            )
 
     @commands.command()
     async def ping(self, ctx):
