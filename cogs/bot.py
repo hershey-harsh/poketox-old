8bf5de0c1d5de1e2a3cd73593db304ff0a63d43b
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-04-20 21:45:16 -0400

    Update bot.py

--- cogs/bot.py
+++ cogs/bot.py
@@ -11,47 +11,17 @@ class Error_Hand(commands.Cog):
         self.bot = bot
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.NoPrivateMessage):
-            await ctx.send("This command cannot be used in private messages.")
-        elif isinstance(error, commands.DisabledCommand):
-            await ctx.send("Sorry. This command is disabled and cannot be used.")
-        elif isinstance(error, commands.BotMissingPermissions):
+        if isinstance(error, commands.BotMissingPermissions):
             missing = [
-                "`" + perm.replace("_", " ").replace("guild", "server").title() + "`"
-                for perm in error.missing_permissions
+                f"`{perm.replace('_', ' ').replace('guild', 'server').title()}`" for perm in error.missing_permissions
             ]
             fmt = "\n".join(missing)
             message = (
                 f"💥 Err, I need the following permissions to run this command:\n{fmt}\nPlease fix this and try again."
             )
-            if ctx.channel.permissions_for(ctx.me).send_messages:
+            botmember = self.bot.user if ctx.guild is None else ctx.guild.get_member(self.bot.user.id)
+            if ctx.channel.permissions_for(botmember).send_messages:
                 await ctx.send(message)
-                
-        elif isinstance(error, commands.MissingPermissions):
-            missing = [
-                "`" + perm.replace("_", " ").replace("guild", "server").title() + "`"
-                for perm in error.missing_permissions
-            ]
-            fmt = "\n".join(missing)
-            message = (
-                f"💥 Err, I need the following permissions to run this command:\n{fmt}\nPlease fix this and try again."
-            )   
-            
-        elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send_help(ctx.command)
-        elif isinstance(error, commands.CommandOnCooldown):
-            await ctx.send(
-                f"You're on cooldown! Try again in **{time.human_timedelta(timedelta(seconds=error.retry_after))}**."
-            )
-        elif isinstance(error, commands.CheckFailure):
-            await ctx.send(error)
-        elif isinstance(error, commands.UserInputError):
-            await ctx.send(error)
-        elif isinstance(error, commands.CommandNotFound):
-            return
-        else:
-            print(f"Ignoring exception in command {ctx.command}")
-            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
     @commands.Cog.listener()
     async def on_error(self, event, error):
