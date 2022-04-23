34c0876de3dfc46ea1926ba956b6e7a3a5ce5a96
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-04-23 12:34:33 -0400

    Update feedback.py

--- cogs/feedback.py
+++ cogs/feedback.py
@@ -7,28 +7,36 @@ from discord.ui import Modal, TextInput
 # of this class is called when the user submits the modal
 class MyModal(Modal):
     def __init__(self) -> None:
-        super().__init__("My Cool Form 1")
+        super().__init__("Pokétox Feedback")
 
-        # Set the questions that will be shown in the modal
-        # The placeholder is what will be shown when nothing is typed
-        self.add_item(TextInput(label="What is your name?", placeholder="Reveal your secrets!"))
-
-        # Provide value argument to prefill the input
-        # The style parameter allows you to set the style of the input
-        # You can choose from short and long
+        self.add_item(TextInput(label="What is your User ID?", placeholder="790788488983085056"))
+        
         self.add_item(
             TextInput(
-                label="What is the meaning of life?",
-                value="The meaning of life is ",
+                label="What is your favorite thing about Pokétox?",
+                placeholder="Identify Spawns, Automatic Pings, Price Check...",
                 style=discord.TextInputStyle.long,
             )
         )
-
+        
+        self.add_item(
+            TextInput(
+                label="What is your **least** favorite thing about Pokétox?",
+                placeholder="Identify Spawns, Automatic Pings, Price Check...",
+                style=discord.TextInputStyle.long,
+            )
+        )
+        
+        self.add_item(
+            TextInput(
+                label="What is your favorite thing about Pokétox?",
+                placeholder="Identify Spawns, Automatic Pings, Price Check...",
+                style=discord.TextInputStyle.long,
+            )
+        )
+        
     async def callback(self, interaction: discord.Interaction):
-        # Use the interaction object to send a response message containing
-        # the user's name or choice. The self object refers to the
-        # Modal object, and the values attribute gets a list of the user's
-        # answers. We only want the first one.
+
         await interaction.response.send_message(
             f"Your name is {self.children[0].value}\n" f"The meaning of life is {self.children[1].value}\n"
         )
