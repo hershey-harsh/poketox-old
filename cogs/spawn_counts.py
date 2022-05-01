3e43a5347904ffb6410bb2660da97d3501a36ab3
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-05-01 12:40:03 -0400

    Update spawn_counts.py

--- cogs/spawn_counts.py
+++ cogs/spawn_counts.py
@@ -3,48 +3,6 @@ from discord.ext import commands
 import requests
 from easy_pil import Editor, Canvas, load_image_async, Font
 
-def make_card(name, xp, percentage, urll):
-    user_data = {  # Most likely coming from database or calculation
-        "name": name,  # The user's name
-        "xp": xp,
-        "percentage": percentage,
-    }
-
-    background = Editor(Canvas((934, 282), "#23272a"))
-
-    response = requests.get(str(urll))
-    file = open("pfp.png", "wb")
-    file.write(response.content)
-    file.close()
-    profile = Editor("pfp.png").resize((190, 190)).circle_image()
-
-    poppins = Font.poppins(size=30)
-
-    background.rectangle((20, 20), 894, 242, "#2a2e35")
-    background.paste(profile, (50, 50))
-    background.ellipse((42, 42), width=206, height=206, outline="#43b581", stroke_width=10)
-    background.rectangle((260, 180), width=630, height=40, fill="#484b4e", radius=20)
-    background.bar(
-        (260, 180),
-        max_width=630,
-        height=40,
-        percentage=user_data["percentage"],
-        fill="#00fa81",
-        radius=20,
-    )
-    background.text((270, 120), user_data["name"], font=poppins, color="#00fa81")
-    background.text(
-        (870, 125),
-        f"{user_data['xp']} / 750",
-        font=poppins,
-        color="#00fa81",
-        align="right",
-    )
-
-    #background.multicolor_text((850, 30), texts=rank_level_texts, align="right")
-    file = File(fp=background.image_bytes, filename="card.png")
-    return file
-
 class spawn_counts(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
@@ -61,6 +19,45 @@ class spawn_counts(commands.Cog):
         percentage = int((x % 1) * 100 // 1)
         file = make_card(ctx.guild, spawn_co, percentage, ctx.guild.icon.url)
         
+        user_data = {  # Most likely coming from database or calculation
+            "name": ctx.guild.name,  # The user's name
+            "xp": spawn_co,
+            "percentage": percentage,
+        }
+
+        background = Editor(Canvas((934, 282), "#23272a"))
+
+        response = requests.get(str(ctx.guild.icon.url))
+        file = open("pfp.png", "wb")
+        file.write(response.content)
+        file.close()
+        profile = Editor("pfp.png").resize((190, 190)).circle_image()
+
+        poppins = Font.poppins(size=30)
+
+        background.rectangle((20, 20), 894, 242, "#2a2e35")
+        background.paste(profile, (50, 50))
+        background.ellipse((42, 42), width=206, height=206, outline="#43b581", stroke_width=10)
+        background.rectangle((260, 180), width=630, height=40, fill="#484b4e", radius=20)
+        background.bar(
+            (260, 180),
+            max_width=630,
+            height=40,
+            percentage=user_data["percentage"],
+            fill="#00fa81",
+            radius=20,
+        )
+        background.text((270, 120), user_data["name"], font=poppins, color="#00fa81")
+        background.text(
+            (870, 125),
+            f"{user_data['xp']} / 750",
+            font=poppins,
+            color="#00fa81",
+            align="right",
+        )
+
+        #background.multicolor_text((850, 30), texts=rank_level_texts, align="right")
+        file = File(fp=background.image_bytes, filename="card.png")
         await ctx.send(file=file)
 
 
