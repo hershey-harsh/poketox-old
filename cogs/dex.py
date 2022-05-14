3591a89be1cc67de2857ba263eb5d3577353838e
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-05-13 20:12:40 -0400

    Update dex.py

--- cogs/dex.py
+++ cogs/dex.py
@@ -1,30 +1,35 @@
-import discord
-from discord.ext import commands, menus
-from helpers import checks
-
-class dex(commands.Cog):
-    """Check pokedex."""
-
-    def __init__(self, bot):
+class Confirm(discord.ui.View):
+    def __init__(self, url, species, name_poke, bot):
+        super().__init__()
+        self.value = None
+        self.url = url
+        self.species = species
         self.bot = bot
-
-    @checks.has_started()
-    @commands.hybrid_command()
-    async def dex(self, ctx, species):
-        """Show Pokédex info"""
+        self.name_poke = name_poke
         
-        shiny = False
+    @discord.ui.button(label="Incorrect Prediction", style=discord.ButtonStyle.red, emoji="<:notify:965755380812611614>")
+    async def predi(self, interaction: discord.Interaction, button: discord.ui.Button):
+                embed=discord.Embed(title="Reported Image", description="Thanks for reporting the image!", color=0x2F3136)
 
+                await interaction.response.send_message(embed=embed,ephemeral=True)
+               
+                data = {"content" : f'Reported by: **{interaction.user.name}** *({interaction.user.id})* {self.url}',"username" : "Incorrect Prediction"}
+
+                url = "https://discord.com/api/webhooks/936421747102744666/1UUkTqapNUlsYTZqKkR_s_EL4IwniPL4w9VKlL_QfMh8FV9zwpm0bkUkVsXA3est57T1"
+
+                requests.post(url, json = data)
+        
+    @discord.ui.button(label="Shiny", style=discord.ButtonStyle.blurple, emoji="✨")
+    async def info(self, interaction: discord.Interaction, button: discord.ui.Button):
+
+        species = self.species
+      
         if species.isdigit():
             species = self.bot.data.species_by_number(int(species))
         else:
-            if species.lower().startswith("shiny "):
-                shiny = True
-                species = species[6:]
-
+            
+            shiny = True
             species = self.bot.data.species_by_name(species)
-            if species is None:
-                return await ctx.send(f"Could not find a pokemon matching `{species}`.")
 
         embed = discord.Embed(color=0x2F3136)
         embed.title = f"#{species.dex_number} — {species}"
@@ -54,9 +59,8 @@ class dex(commands.Cog):
         if species.evolution_text:
             embed.add_field(name="Evolution", value=species.evolution_text, inline=False)
 
-        if shiny:
-            embed.title = f"#{species.dex_number} — ✨ {species}"
-            embed.set_thumbnail(url=species.shiny_image_url)
+        if 1 == 2:
+            print("idk")
         else:
             embed.set_thumbnail(url=species.image_url)
 
@@ -76,8 +80,4 @@ class dex(commands.Cog):
         embed.add_field(name="Base Stats", value="\n".join(base_stats))
         embed.add_field(name="Types", value="\n".join(species.types))
 
-        await ctx.send(embed=embed)
-    
-async def setup(bot):
-  print("Loaded Dex")
-  await bot.add_cog(dex(bot))
+        await interaction.response.send_message(embed=embed,ephemeral=True)
