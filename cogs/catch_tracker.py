6e7892839daf1f5ff4a2d94b52374d06ca111bc6
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-05-15 07:02:32 -0400

    Update catch_tracker.py

--- cogs/catch_tracker.py
+++ cogs/catch_tracker.py
@@ -35,7 +35,7 @@ class catch_log(commands.Cog):
     @commands.hybrid_command(brief="Disable Pings and Catch Logs")
     async def disable(self, ctx, select: Literal['Pings', 'Catch Logs'], serverid: Optional[str] = None):
 
-        guildid = ServerID
+        guildid = serverid
       
         if guildid == None:
           guildid = ctx.guild.id      
@@ -77,6 +77,56 @@ class catch_log(commands.Cog):
             await ctx.send(embed=embed)
           else:
             embed=discord.Embed(title="Ping", description=f"This feature is already disabled in **{ctx.guild}**!", color=0x36393F)
+            embed.set_thumbnail(url=ctx.guild.icon.url)
+            await ctx.send(embed=embed)
+            
+    @checks.has_started()
+    @commands.cooldown(1, 3, commands.BucketType.user)
+    @commands.hybrid_command(brief="Disable Pings and Catch Logs")
+    async def enable(self, ctx, select: Literal['Pings', 'Catch Logs'], serverid: Optional[str] = None):
+
+        guildid = serverid
+      
+        if guildid == None:
+          guildid = ctx.guild.id      
+      
+        if select == "Pings":
+          
+          result = await self.bot.mongo.db.collector.update_one(
+              {"_id": ctx.author.id},
+              {"$unset": {str(ctx.guild.id): True}},
+              upsert=True,
+          )
+        
+          result = await self.bot.mongo.db.shinyhunt.update_one(
+              {"_id": ctx.author.id},
+              {"$unset": {str(ctx.guild.id): True}},
+              upsert=True,
+          )
+
+          if result.upserted_id or result.modified_count > 0:
+            embed=discord.Embed(title="Collector", description=f"You will get pinged when your shiny hunt spawns or what your collecting in **{ctx.guild}**", color=0x36393F)
+            embed.set_thumbnail(url=ctx.guild.icon.url)
+            await ctx.send(embed=embed)
+          else:
+            embed=discord.Embed(title="Ping", description=f"This feature is already enabled in **{ctx.guild}**!", color=0x36393F)
+            embed.set_thumbnail(url=ctx.guild.icon.url)
+            await ctx.send(embed=embed)
+            
+        if select == "Catch Log":
+          
+          result = await self.bot.mongo.db.catchlog.update_one(
+              {"_id": ctx.author.id},
+              {"$unset": {str(ctx.guild.id): 1}},
+              upsert=True,
+          )
+
+          if result.upserted_id or result.modified_count > 0:
+            embed=discord.Embed(title="Catch Log", description=f"Your spawns will be tracked in in **{ctx.guild}**", color=0x36393F)
+            embed.set_thumbnail(url=ctx.guild.icon.url)
+            await ctx.send(embed=embed)
+          else:
+            embed=discord.Embed(title="Ping", description=f"This feature is already enabled in **{ctx.guild}**!", color=0x36393F)
             embed.set_thumbnail(url=ctx.guild.icon.url)
             await ctx.send(embed=embed)
         
