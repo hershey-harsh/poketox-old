64ee4dabcf2be2f6e36d88e1f493eb49e020ebbf
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-05-25 21:30:55 -0400

    Update bot.py

--- cogs/bot.py
+++ cogs/bot.py
@@ -115,18 +115,14 @@ class Error_Hand(commands.Cog):
         total_members = 0
         for guild in self.bot.guilds:
             total_members += guild.member_count
+            total_members = "{:,}".format(int(total_members))
             
         registered = await self.bot.mongo.db.member.estimated_document_count()
-        total_registred = registered + 50000
+        total_registred = registered + 200000
+        total_registred = "{:,}".format(int(total_registred))
         
         msg = f"""
-        **Live Status**
-        Next update {discord.utils.format_dt(self.edit_status.next_iteration, 'R')}
-        
-        Ping: {round (self.bot.latency * 1000)}ms
-        Servers: {len(self.bot.guilds)}
-        Members: {total_members + 200000}
-        Registered Users: {total_registred}
+        **Live Status**\nNext update {discord.utils.format_dt(self.edit_status.next_iteration, 'R')}\n\nPing: {round (self.bot.latency * 1000)}ms\nServers: {len(self.bot.guilds)}\nMembers: {total_members + 200000}\nRegistered Users: {total_registred}
         """
         
         await self.message.edit(content=msg)
