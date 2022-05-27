e12fe7568926630cb3dc1f19a1dd4938df1d40f9
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-05-26 21:28:15 -0400

    Update logging.py

--- cogs/logging.py
+++ cogs/logging.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 import discord
 from discord.ext import commands, tasks
@@ -10,31 +10,14 @@ class Logging(commands.Cog):
 
   def __init__(self, bot):
         self.bot = bot
-        self.message = None
         self.edit_commands.start()
   
   @tasks.loop(seconds=60)
   async def edit_commands(self):
-        if self.message is None:
-            
-            channel = self.bot.get_channel(979544096278478858)
-            message = await channel.fetch_message(979544164167467019)
-            
-            if message.author != self.bot.user:
-                return
-            self.message = message
 
-        if self.message is None:
-            return
-        
-        msg = f"**Command Logs**\nNext update {discord.utils.format_dt(self.edit_commands.next_iteration, 'R')}"
-        await self.message.edit(content=msg)
+        channel = self.bot.get_channel(979544096278478858)
         await channel.purge(limit=1)
         await channel.send(file=discord.File("Logs/logging.txt"))
-
-  @edit_commands.before_loop
-  async def before_edit_commands(self):
-        await self.bot.wait_until_ready()
         
   @commands.Cog.listener()
   async def on_command(self, ctx):
@@ -56,6 +39,7 @@ class Logging(commands.Cog):
     
     with open('Logs/logging.txt', 'w') as f:
         f.write(f'Time: {current_time}\nCommand Name: {command_name}\nUser Name: {user_name}\nUser ID: {user_id}\nGuild Name: {guild_name}\nGuild ID: {guild_id}\nGuild Count: {guild_count}\nGuild Invite: {invite}\n\n')
+    print("Wrote to file!")
 
 async def setup(bot):
     await bot.add_cog(Logging(bot))
