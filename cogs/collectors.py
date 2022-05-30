dc9f7fa5b1632bc704ccbe59ca227dd47fc35164
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-05-30 19:52:02 -0400

    Update collectors.py

--- cogs/collectors.py
+++ cogs/collectors.py
@@ -53,6 +53,7 @@ async def collectping(self, ctx, species: SpeciesConverter):
                 x = datetime.now() + timedelta(seconds=3)
                 x += timedelta(seconds=int(time))
                 time = discord.utils.format_dt(x, 'R')
+                await ctx.send(time)
             else:
                 time = " "
                 
