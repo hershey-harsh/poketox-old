66278b50795182c8f6a2b9c2c5cc6217b649a98a
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-05-31 21:50:58 -0400

    Update name.py

--- name.py
+++ name.py
@@ -24,7 +24,7 @@ def solve(url):
   
 async def identifyy(url):
   async with aiohttp.ClientSession() as session:
-        pokemon_url = f'http://137.184.156.29:8080/identify/{url}'
+        pokemon_url = f'http://68.183.61.195:8080/identify/{url}'
         async with session.get(pokemon_url) as resp:
             pokemon = await resp.text()
             return pokemon
