a41fb64f6b56c67df832a92aa5196b092b588817
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-07-03 17:34:12 -0400

    Update bot.py

--- cogs/bot.py
+++ cogs/bot.py
@@ -121,8 +121,8 @@ class Error_Hand(commands.Cog):
                 "collectlist globalsearch" : "https://docs.poketox.me/collectlist-pings/collectlist-globalsearch",
             }
             try:
-            embed=discord.Embed(title="Command Error", description=f"({ctx.command.name})[{commands_link[str(ctx.command.name)]}]", color=0x2F3136)
-            await ctx.send(embed=embed)
+                embed=discord.Embed(title="Command Error", description=f"({ctx.command.name})[{commands_link[str(ctx.command.name)]}]", color=0x2F3136)
+                await ctx.send(embed=embed)
             except Exception as e:
                 print(e)
         
