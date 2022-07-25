104ee1dc9b37e219a4ab10cfb6f02d8503846229
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-07-25 14:42:45 -0400

    Update pokedex.py

--- cogs/pokedex.py
+++ cogs/pokedex.py
@@ -537,14 +537,13 @@ class Pokedex(commands.Cog):
           try:
             if pokemon in total_rare_pokes:
                 star_channel = self.bot.get_channel(int(guild["starboard"]))
-                embed=discord.Embed(title=f"A wild {species.name} has appeared!", description=f"{species.lower} was spawned in {message.channel.id} {timest}", color=0x2f3136)
+                embed=discord.Embed(title=f"A wild {species.name} has appeared!", description=f"{species.lower} was spawned in {message.channel.id}", color=0x2f3136)
                 embed.set_thumbnail(url=species.image_url)
                 await star_channel.send(embed=embed, view=Jump(plan))
           except:
             pass
     
   async def identify(self, img_url, message, plan):
-    timest = discord.utils.format_dt(datetime.now(), 'R')
     ctx = await self.bot.get_context(message)
     guild = await ctx.bot.mongo.fetch_guild(ctx.guild)
     
@@ -632,7 +631,7 @@ class Pokedex(commands.Cog):
           try:
             if pokemon in total_rare_pokes:
                 star_channel = self.bot.get_channel(int(guild["starboard"]))
-                embed=discord.Embed(title=f"A wild {species.name} has appeared!", description=f"{species.lower} was spawned in {message.channel.id} {timest}", color=0x2f3136)
+                embed=discord.Embed(title=f"A wild {species.name} has appeared!", description=f"{species.lower} was spawned in {message.channel.id}", color=0x2f3136)
                 embed.set_thumbnail(url=species.image_url)
                 await star_channel.send(embed=embed, view=Jump(plan))
           except:
