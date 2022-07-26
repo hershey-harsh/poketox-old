7a5fbedc3cf3d903bb15fa7f3eb8a4fe1ea785af
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-07-26 11:37:01 -0400

    Update predict_app.py

--- cogs/predict_app.py
+++ cogs/predict_app.py
@@ -89,7 +89,13 @@ class predict_app(commands.Cog):
         self.bot.tree.remove_command(self.ctx_menu.name, type=self.ctx_menu.type)
         
     async def app_identify(self, interaction: discord.Interaction, message: discord.Message) -> None:
-        pokemon = await identifyy(message.content)
+        if message.embeds:
+            url = message.embeds[0].image.url
+            
+         else:
+            url = message.content
+        
+        pokemon = await identifyy(url)
         species = self.bot.data.species_by_name(pokemon)
         
         if species is None:
