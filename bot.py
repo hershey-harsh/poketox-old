fe6bc728c9e9437f20f312971dcbeb9c64d38de4
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-08-05 22:12:19 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import os
 import discord
-from discord.ext import commands, events
+from discord.ext import commands, events, ipc
 from discord.ext.events import member_kick
 import datetime
 import helpers
 from helpers import checks
 from helpers import context
 import aiohttp
+from termcolor import colored, cprint
 import config
 
 COGS = [
@@ -29,7 +30,7 @@ COGS = [
     "admin",
     "feedback",
     "economy",
-    "predict_app"
+    "predict_app",
 ]
 
 intents = discord.Intents.default()
@@ -37,12 +38,15 @@ intents.message_content = True
 
 class Bot(commands.Bot, events.EventsMixin):
     def __init__(self, **kwargs):
+        
         super().__init__(
             **kwargs,
             command_prefix=[config.PREFIX, "A!"], self_bot=False, owner_id = 790788488983085056, slash_commands=True, intents=intents,
             allowed_mentions=discord.AllowedMentions(everyone=False, roles=True, replied_user=False),
             case_insensitive=True,
         )
+        
+        self.ipc = ipc.Server(self, secret_key="my_secret_key")
       
         self.config = config
         os.system("clear")
@@ -64,7 +68,16 @@ class Bot(commands.Bot, events.EventsMixin):
                 external_emojis=True,
             ).predicate
         )
-
+        
+    os.system("clear")
+    
+    async def on_ipc_ready(self):
+        """Called upon the IPC Server being ready"""
+        cprint("IPC Server is online.", 'green')
+        
+    async def on_ipc_error(self, endpoint, error):
+        """Called upon an error being raised within an IPC route"""
+        print(endpoint, "raised", error)
         
     @property
     def mongo(self):
@@ -79,5 +92,5 @@ class Bot(commands.Bot, events.EventsMixin):
 
 if __name__ == "__main__":
     bot = Bot()
-    os.system('clear')
+    bot.ipc.start()
     bot.run(config.BOT_TOKEN)
