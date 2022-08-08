99d15fbd9ba9354d8ab580b1ff9a20f3b1792466
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-08-08 14:21:56 -0400

    Update ipc.py

--- cogs/ipc.py
+++ cogs/ipc.py
@@ -8,34 +8,33 @@ from discord.ext.ipc.server import route
 from discord.ext.ipc.errors import IPCError
 from flask import Flask
 
-app = Flask('')
+from aiohttp import web
+from discord.ext import commands, tasks
+import discord
+import os
+import aiohttp
+
+app = web.Application()
 
 class Routes(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        if not hasattr(bot, "ipc"):
-            bot.ipc = ipc.Server(self.bot, host="134.209.35.31", port=5000, secret_key="2022poketox2007")
-            bot.ipc.start()
-        for name, function in inspect.getmembers(self):
-            if name.startswith("get_"): # ATTENTION: Every function that stats with `get_` will be registered as endpoint
-                bot.ipc.endpoints[name] = function
+        self.web_server.start()
     
-    @commands.Cog.listener()
-    async def on_ipc_ready(self):
-        logging.info("Ipc is ready")
-    
-    @commands.Cog.listener()
-    async def on_ipc_error(self, endpoint: str, error: IPCError):
-        logging.error(endpoint, "raised", error, file=sys.stderr)
-
-    async def get_user_data(self, data):
-        user = self.bot.get_user(data.user_id)
-        return user._to_minimal_user_json() # THE OUTPUT MUST BE JSON SERIALIZABLE!
-    
-    @app.route('/test')
+    @routes.get('/test')
     def home():
         return "I'm alive"
     
+    @tasks.loop()
+    async def web_server(self):
+        runner = web.AppRunner(app)
+        await runner.setup()
+        site = web.TCPSite(runner, host='134.209.35.31', port=5000)
+        await site.start()
+        
+    @web_server.before_loop
+    async def web_server_before_loop(self):
+        await self.bot.wait_until_ready()
 
 async def setup(bot):
     await bot.add_cog(Routes(bot))
