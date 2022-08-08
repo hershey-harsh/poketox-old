90eaa52d73a8b2eafa2c475deea0b1d80fc9d455
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-08-08 11:20:46 -0400

    Update ipc.py

--- cogs/ipc.py
+++ cogs/ipc.py
@@ -11,7 +11,7 @@ class Routes(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         if not hasattr(bot, "ipc"):
-            bot.ipc = ipc.Server(self.bot, host="127.0.0.1", port=5000, secret_key="2022poketox2007")
+            bot.ipc = ipc.Server(self.bot, host="134.209.35.31", port=5000, secret_key="2022poketox2007")
             bot.ipc.start()
         for name, function in inspect.getmembers(self):
             if name.startswith("get_"): # ATTENTION: Every function that stats with `get_` will be registered as endpoint
