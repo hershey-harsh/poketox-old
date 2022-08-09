052cec3920165011e3c512b692a6561c5da9e5f4
Harsh <77411358+Futureslinky@users.noreply.github.com>
2022-08-09 11:30:18 -0400

    Update ipc.py

--- cogs/ipc.py
+++ cogs/ipc.py
@@ -11,7 +11,7 @@ class Routes(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         if not hasattr(bot, "ipc"):
-            bot.ipc = ipc.Server(self.bot, port = 1025, secret_key="2022poketox2007")
+            bot.ipc = ipc.Server(self.bot, host="0.0.0.0", port = 1025, secret_key="2022poketox2007")
             bot.ipc.start()
         
         for name, function in inspect.getmembers(self):
